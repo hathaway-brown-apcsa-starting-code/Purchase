bca9142059f10a8dfb7a6896aef321924b9ac67c
mbuescher <mbuescher@hb.edu>
2019-09-09 06:02:46 -0400

    upload TestPurchase.java

--- TestPurchase.java
+++ TestPurchase.java
@@ -20,8 +20,14 @@ public class TestPurchase
         
         Purchase p1 = new Purchase (name, num, price);
 
-        System.out.println("\n");
+        System.out.println("\nTesting toString():");
         System.out.println(p1);
+        
+        System.out.println("Testing individual accessor methods:");
+        System.out.println("Testing getItemName: " + p1.getItemName() );
+        System.out.println("Testing getQuantity: " + p1.getQuantity() );
+        System.out.println("Testing getPrice: " + p1.getPrice() );
+        System.out.println("Testing getTotalCost: " + p1.getTotalCost() );
         
         kbd.close();
     }
